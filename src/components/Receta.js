568c302514a903fb5e3117d9e7a5bbe17ee5902c
Mauri <mauricio.stampella@25watts.com.ar>
2021-01-24 23:00:18 -0300

    Importando el componente de Modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,31 +1,80 @@
-import React, {useContext} from 'react';
-import {ModalContext} from '../context/ModalContext'
+import React, { useContext, useState } from 'react';
+import { ModalContext } from '../context/ModalContext'
 
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Receta = ({receta}) => {
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
 
-    // extraer los valores del Context
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
+const Receta = ({ receta }) => {
+
+    // Configuración del modal de material-ui
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    // Extraer los valores del Context
     const { guardarIdReceta } = useContext(ModalContext);
 
-    return ( 
+    return (
         <div className="col-md-4 mb-3">
             <div className="card">
                 <h2 className="card-header">{receta.strDrink}</h2>
                 <img className="card-img-top" src={receta.strDrinkThumb} alt={`Imagen de ${receta.strDrink}`}></img>
                 <div className="card-body">
-                    <button 
+                    <button
                         type="button"
                         className="btn btn-block btn-primary"
-                        onClick={()=>{
-                            guardarIdReceta(receta.idDrink)
+                        onClick={() => {
+                            guardarIdReceta(receta.idDrink);
+                            handleOpen();
                         }}
                     >
                         Ver Receta
                     </button>
+
+                    <Modal
+                        open={open}
+                        onClose={() => {
+                            guardarIdReceta(null);
+                            handleClose();
+                        }}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h1>desde modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Receta;
